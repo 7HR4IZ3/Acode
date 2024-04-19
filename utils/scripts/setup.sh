ab85ae81871a0a9dacd825fd91f069ed0be2a10e
7HR4IZ3 <gamerxville@gmail.com>
2024-04-19 16:40:38 +0100

    fix: Fixed github workflow

--- utils/scripts/setup.sh
+++ utils/scripts/setup.sh
@@ -1,13 +1,18 @@
 echo "Setting up the project..."
 
-npm install
+echo "Setting up Android NDK..."
 sudo apt-get install android-sdk
 export ANDROID_HOME=/usr/lib/android-sdk
 sudo apt install sdkmanager
+
+echo "Setting up Android..."
 cordova platform add android@10
+
+echo "Setting up cordova plugins..."
 cordova plugin add cordova-plugin-buildinfo
 cordova plugin add cordova-plugin-device
 cordova plugin add cordova-plugin-file
+cordova plugin add https://github.com/7HR4IZ3/nodejs-mobile-cordova
 
 PLATFORM_FILES='.DS_Store'
 PLUGINS_DIR="../src/plugins"
@@ -18,6 +23,7 @@ for plugin in $(ls $PLUGINS_DIR); do
   fi
   cordova plugin add "$PLUGINS_DIR/$plugin"
 done
+
 cordova prepare
 sdkmanager "build-tools;30.0.3"
 mkdir -p www/css/build www/js/build
